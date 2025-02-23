497804ec21550937b65ea844a132fe377badce12
john <jinyngg@gmail.com>
2025-02-23 23:18:54 +0900

    [#9] feat : password 검증 어노테이션 작성

---
+++ api/src/main/java/com/pp/annotation/PasswordMatch.java
@@ -0,0 +1,20 @@
+package com.pp.annotation;
+
+import com.pp.validator.PasswordMatchValidator;
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.*;
+
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = PasswordMatchValidator.class)
+@Documented
+public @interface PasswordMatch {
+    String message() default "비밀번호가 일치하지 않습니다.";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+
+    String password();
+    String confirmPassword();
+}
